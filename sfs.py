b3301c022e8e32e6ed531faff450896481ca30db
SovietSherlock <savvabaryshev@gmail.com>
2023-01-17 15:26:01 +0300

    17.01.2023

---
+++ sfs.py
@@ -0,0 +1,48 @@
+# my_set = {5, 1, 2, 6, 3, 4, 3, 2}
+# s = [5, 1, 2, 6, 3, 4, 3, 2]
+# print(s)
+# my_set.add(4)
+# print(my_set)
+# my_set.add(4)
+# print(my_set, len(my_set), sum(my_set))
+
+# a={} - создание пустого множества
+
+# ТИП а
+# print(type(a)) - узнать тип а
+
+# UPDATE
+# my_set = {5, 1, 2, 6, 3, 4, 3, 2}
+# my_set.add('Hello, World')
+# my_set.update((20, 30), [152, 153], {0, -1})
+# print(my_set)
+
+# УДАЛЕНИЕ remove(), discard()
+# my_set = {5, 1, 2, 6, 3, 4, 3, 2}
+# my_set.remove(1)
+# my_set.discard(6)
+# print(my_set)
+
+# МЕТОД pop() - удаляет случайный элемент
+# my_set = {5, 1, 2, 6, 3, 4, 3, 2}
+# my_set.remove(1)
+# my_set.discard(6)
+# a = my_set.pop()
+# my_set.clear()
+# print(my_set)
+
+# МЕТОД union()
+# A = {1, 2, 3, 4, 5}
+# B = {4, 5, 6, 7, 8}
+# print(A | B)
+# A = {1, 2, 3, 4, 5}
+# B = {4, 5, 6, 7, 8}
+# C = A | B
+# A.union(B)
+# B.union(A)
+# print(A & B)
+
+# МЕТОД interseption()
+
+
+
