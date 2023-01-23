576d46bb4e0e6958518e8e25e10c33a0a444c042
Данила Зименский <zimenskiy17@gmail.com>
2023-01-23 18:40:54 +0300

    egehka

--- 2.py
+++ 2.py
@@ -1,11 +1,10 @@
 # ((x → y) ≡ (y → z)) ∧ (y ∨ w)
 
-'''
+
 print('x y z w')
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if (((x <= y) == (y <= z)) and (y or w)) == 1:
+                if ((x or not(z)) and (x == (not(w))) and (x <= y)) == 1:
                     print(x, y, z, w)
-'''
