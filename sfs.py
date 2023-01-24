7e079dfedd9cf43bb170a5c973013f2131a352a4
Student <kbn b>
2023-01-24 15:28:08 +0300

    24.01

--- sfs.py
+++ sfs.py
@@ -45,4 +45,41 @@
 # МЕТОД interseption()
 
 
+# СЛОВАРИ
 
+# пустой словарь
+# my_dicts = {}
+
+# словарь с ключами
+# my_dicts = {'имя': 'Джон', 1: [2,4,6]}
+
+
+'''my_dict = dict([[1, 'яблоко'], [2, 'мяч']])
+print(my_dict)'''
+
+'''sp = {1: 'яблоко', 2: 'груша', 3: 'апельсин'} # сделать список
+   my_dict = dict(sp) # превратить список в словарь
+   my_dict[3] = 'мандарин' # добавление элемента
+   my_dict[4] = 'слива' # добавление элемента
+   my_dict.pop(1) # удаление элемента
+   # del my_dict # удаление всего словоря
+   print(my_dict.items()) # вывести словарь'''
+
+'''my_dict = {1: 'яблоко', 2: 'груша', 3: 'апельсин'}
+   print(my_dict.items())
+   print(my_dict.keys())
+   print(my_dict.values())'''
+
+# ГЕНЕРАТОР СЛОВАРЕЙ
+'''squares = {x: x*x for x in range(6)}
+   print(squares)'''
+
+'''squares = {}
+   for x in range(6):
+     squares[x] = x*x
+   print(squares)'''
+
+'''squares = {x: x*x for x in range(10) if x % 2 == 0}
+   print(squares)'''
+
+# CTRL+ALT+L - PEP8
