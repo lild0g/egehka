221022b992eb3d82d443c71db086ad8a603380c7
Student <lady.khodina@yandex.ru>
2023-02-14 16:24:56 +0300

    vfgv

--- теория игр.py
+++ теория игр.py
@@ -16,20 +16,16 @@ for s in range(1, 128 + 1):
 
 # 19 задание с двумя кучами
 
-'''def f(s1, s2, c, m):
-    if (s1 + s2) >= 77:
-        return c % 2 == m % 2
-    if c == m:
-        return 0
+# номер 27759 решу егэ
+def f(s1, s2, c, m):
+    if (s1 + s2) >= 41: return c % 2 == m % 2
+    if c == m: return 0
     h = [f(s1 + 1, s2, c + 1, m), f(s1 * 2, s2, c + 1, m), f(s1, s2 + 1, c + 1, m), f(s1, s2 * 2, c + 1, m)]
-    return any(h) if (c + 1) % 2 == m % 2 else any(h)  # при любом ходе Пети all()
-    # при неудачном ходе Пети any()
+    return any(h) if (c + 1) % 2 == m % 2 else any(h)
 
 
-for s2 in range(1, 69 + 1):
+for s2 in range(1, 33 + 1):
     for m in range(1, 4 + 1):
-        if f(7, s2, 0, m) == 1:
-            print(s2, m)'''
-
-
-
+        if f(8, s2, 0, m) == 1:
+            print(s2, m)
+            break
