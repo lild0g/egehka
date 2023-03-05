ab4b64b0c382512e746ccc55f58cead9e0fce681
Данила Зименский <zimenskiy17@gmail.com>
2023-03-05 15:41:07 +0300

    /

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 # ((x → y) ≡ (y → z)) ∧ (y ∨ w)
-
+# ((x → y ) ≡ (z → w)) ∨ (x ∧ w).
 
 print('x y z w')
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if ((x or not(z)) and (x == (not(w))) and (x <= y)) == 1:
+                if not(((x <= y) == (z <= w)) or (x and w)):
                     print(x, y, z, w)
