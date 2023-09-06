703787ee356a9a35139eab9ea9baeb19b8f7bac7
Student <zimenskiy17@gmail.com>
2023-09-06 14:17:44 +0300

    g

--- 24.py
+++ 24.py
@@ -26,3 +26,44 @@ for i in range(1, len(s)):
         if k > m:
             m = k
 print(m)'''
+
+
+'''s = open('24_demo.txt').readline()
+s = s.replace('X', ' ').replace('Y', ' ')
+s = s.split()
+print(len(max(s, key = len)))'''#1
+
+'''2s = open('zadanie24_1.txt').readline()
+a = 'AB'*12
+print(len(a))'''#2
+
+'''import string
+al = string.ascii.uppercase
+s = open('24.txt').readline()
+
+for i in al:
+    print(i, s.count(f'A{i}'))'''#3
+
+'''s = open('24.txt').readline()
+
+
+
+'''#4
+
+'''s = open('24 (2).txt').readline()
+while 'XZZY' in s:
+    s = s.replace('XZZY', 'XZZ ZZY')
+print(max(len(c) for c in s.split()))'''#5
+
+'''s = open('24.txt').readline()
+s = s.replace('KL', 'K L').replace('LK', 'L K')
+print(max(len(c) for  c in s.split()))'''#6
+
+'''s = open('24.txt').readline()
+
+'''#7
+
+'''s = open('24.txt').readline()
+s = s.replace('O', 'A').replace('D', 'C').replace('F', 'C')
+s = s.replace('CCA', '*').replace('A', ' ').replace('C', ' ')
+print(max(len(c) for c in s.split()))''' #8
