14950e593d2e28c9b0b4ec2c09f5f759ed46d6e0
Student <zimenskiy17@gmail.com>
2023-11-20 15:24:32 +0300

    bb

--- 8.py
+++ 8.py
@@ -8,12 +8,36 @@
 
 
 
-
-from itertools import product
+'''from itertools import product
 a = product('ABCDEFX', repeat=4)
 k = 0
 for i in a:
     s = ''.join(i)
     if s.count('X') == 1:
         k += 1
-print(k)
+print(k)'''
+
+# Номер 6926 Поляков
+'''from itertools import *
+k = 0
+for i in product('чистыйразум', repeat = 5):
+    s = ''.join(i)
+    if s.count('й') <= 1:
+        k += 1
+print(k)'''
+
+# Номер 6272
+'''from itertools import *
+k = 0
+for j in range(2, 5):
+    for i in permutations('ABCD', j)
+        s = ''.join(i)
+print(k)'''
+
+# номер 5758
+from itertools import *
+x = []
+for i in product('антитопия', repeat = 16):
+    a = ''.join(i)
+    if 'антиутопия' in a:
+        if
