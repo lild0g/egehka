2b6e2ed68a70ab5966a37ccb4ef352080744429c
Student <zimenskiy17@gmail.com>
2024-03-11 16:25:17 +0300

    ff

--- 15.py
+++ 15.py
@@ -120,10 +120,85 @@ for a1 in range(200, 600):
 print(m//10)'''
 
 
-def f(x, y, a):
+'''def f(x, y, a):
     return (x + 2 * y < a) or (y > x) or (x > 60) #выражение в ()
 
 for a in range(1, 1000):
     if all(f(x, y, a) == 1 for x in range(1, 1000) for y in range(1, 1000)):
         print(a)
-        break
+        break'''
+
+# 22 Поляков
+'''def f(x, A):
+    return ((x & 25) != 0) <= (((x & 17) == 0) <= ((x & A) != 0))
+
+for A in range(0, 1000):
+    if all(f(x, A) == 1 for x in range(0, 1000)):
+        print(A)
+        break'''
+
+# 370 Поляков
+'''def f(x,a):
+    return (x % a != 0) <= ((x % 6 == 0) <= (x % 4 != 0))
+
+for a in range(1, 1000):
+    if all(f(x, a) == 1 for x in range(1, 1000)):
+        print(a)'''
+
+
+# 7267 Поляков
+'''def f(x, y, a):
+    return (4 * x + y > 115) or (x > 3 * y) or (x + 4 * y < a)
+
+
+for a in range(0, 500):
+    if any(f(x, y, a) == 0 for x in range(0, 1000) for y in range(0, 1000)):
+        print(a)'''
+
+
+# 6222 Поляков
+'''def f(x, y, a):
+    return (((x+y) - 73) <= 0) or ((37 - (x-y)) <= 0) or ((y - a) > 0)
+
+
+for a in range(0, 1000):
+    if all(f(x, y, a) == 1 for x in range(0, 1000) for y in range(0, 1000)):
+        print(a)'''
+
+
+# 5921 Поляков
+'''def f(x, y, a):
+    return (x + a >= 160) or ((x % 7 == 0) <= (x - 17 <= 0))
+
+
+for a in range(0, 1000):
+    if all(f(x, y, a) == 1 for x in range(0, 1000) for y in range(0, 1000)):
+        print(a)'''
+
+
+# 5920 Поляков
+'''def f(x, y, z, a):
+    return ((z % 115 == 0) or (y % 78 == 0) or (x % 51 == 0)) <= (x * y * z % a == 0)
+
+
+for a in range(1, 500):
+    if all(f(x, y, z, a) == 1 for x in range(1, 500) for y in range(1, 500) for z in range(1, 500)):
+        print(a)'''
+
+
+'''def f(a, x):
+    return ((37 + a + x + 45) == 180) == ((a + 90 == 180) and not (a + 23 < 120))
+
+
+for a in range(1, 181):
+    if all(f(a, x) == 1 for x in range(1, 1000)):
+        print(a)'''
+
+
+'''def f(x, y, z, a):
+    return (x | 50 == x) or (y & 34 != 0) or (z | 24 != 24) or (x * y * z > (a // 8))
+
+
+for a in range(1, 100):
+    if all(f(x, y, z, a) == 1 for x in range(1, 100) for y in range(1, 100) for z in range(1, 100)):
+        print(a)'''
