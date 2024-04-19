df2ae9ce748c4716e781b81164643b91528ea035
Student <zimenskiy17@gmail.com>
2024-04-19 15:27:44 +0300

    ff

--- 27.py
+++ 27.py
@@ -1,8 +1,16 @@
 # номер 48448 РЕШУ ЕГЭ
-f = open('27-B.txt')
+'''f = open('27-B.txt')
 n = int(f.readline())
 k = [0] * 3
 for x in range(n):
     x = int(f.readline())
     ost = x % 3
-    k[ost] += 1
+    k[ost] += 1'''
+
+# Номер 5789 Поляков
+f = open('27-A.txt')
+n, m = map(int, f.readline().split())
+a = []
+for i in range(n):
+    k = int(f.readline())
+
